14d078bc6dffe52d99488161b8eaf5e6b41937cf
split-pig <2954383503@qq.com>
2022-12-06 22:17:25 +0800

    mytest

--- cpu_top.v
+++ cpu_top.v
@@ -1,23 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 2021/11/16 22:59:36
-// Design Name: 
-// Module Name: top_parts
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 `include "define.vh"
 
